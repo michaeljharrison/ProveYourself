f1df1f5a13b8893579c405eb237f6d75b44ecb40
Mike <Michael.J.Harrison@outlook.com>
2021-09-22 15:56:17 +1000

    Update various settings and add dockerfile.

---
+++ server/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const express = require('express')
+const consola = require('consola')
+const { loadNuxt } = require('nuxt-start')
+const app = express()
+
+async function start() {
+  const nuxt = await loadNuxt(isDev ? 'dev' : 'start')
+  await nuxt.listen(process.env.PORT, process.env.HOST)
+}
+
+start()
