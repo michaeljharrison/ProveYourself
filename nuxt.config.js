542257cf17877c0b8db2588567c22ae712a1807c
Mike <Michael.J.Harrison@outlook.com>
2021-09-22 15:16:05 +1000

    Update server config.

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,11 @@ export default {
   // Server Middleware
   serverMiddleware: [{ path: '/api', handler: '~/server-middleware/rest.ts' }],
 
+  server: {
+    port:  process.env.PORT, // default: 3000
+    host: '0.0.0.0' // default: localhost
+  }
+  
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['@/plugins/antd-ui', '@/plugins/vue-json-pretty'],
 
