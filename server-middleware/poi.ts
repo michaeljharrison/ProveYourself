244921a873dde94d187df5c6c3e6949672336d81
Mike <Michael.J.Harrison@outlook.com>
2021-10-12 13:31:19 +1100

    Add Computer Vision Tests

--- server-middleware/poi.ts
+++ server-middleware/poi.ts
@@ -1,10 +1,17 @@
 import { ReadResult } from '@azure/cognitiveservices-computervision/esm/models'
-import { POI, POI_STATUS, Verification } from '../store/types'
+import { POI_STATUS, Verification } from '../store/types'
 
-export function checkUpload(poi: POI, ocrResult: ReadResult): Verification {
-  const verificationCode = poi.initialProof.proof.metadata.txnId
-    .substring(0, 20)
-    .toUpperCase()
+export function checkUpload(poi: any, ocrResult: ReadResult): Verification {
+  let verificationCode: string
+  if (poi.initialProof) {
+    verificationCode = poi.initialProof.proof.metadata.txnId
+      .substring(0, 20)
+      .toUpperCase()
+  } else {
+    // Workaround for test cases with no proof attached.
+    verificationCode = poi.substring(0, 20).toUpperCase()
+  }
+
   const { lines } = ocrResult
   let verified = POI_STATUS.FAILED
   let verifiedConfidence = 0
@@ -13,6 +20,7 @@ export function checkUpload(poi: POI, ocrResult: ReadResult): Verification {
   let joiningWords = false
   let joinedWord = ''
   // Iterate through each line
+  console.log(lines)
   lines.forEach((line, lineNumber) => {
     // Iterate through each word on that line.
     const { words } = line
@@ -65,165 +73,6 @@ export function checkUpload(poi: POI, ocrResult: ReadResult): Verification {
         // Current word is a starting prefix.
         joiningWords = true
         joinedWord = word.text
-      }
-
-      // known OCr issue with 0 and O, so check both just in case.
-      if (word.text.includes('O')) {
-        word.text = word.text.replace('O', '0')
-        console.log('Trying again with O to 0 replacement.')
-        if (joinedWord) {
-          console.log(`${joinedWord} + ${word.text} === ${verificationCode}`)
-        } else {
-          console.log(`${word.text} === ${verificationCode}`)
-        }
-        // First, see if this word is the entire verification code.
-        if (word.text === verificationCode) {
-          verified = POI_STATUS.VERIFIED
-          verifiedConfidence = word.confidence
-          lineFound = lineNumber
-          wordFound = wordNumber
-          return {
-            verified,
-            verifiedConfidence,
-            lineFound,
-            wordFound,
-            verificationCode,
-          }
-        } else if (joiningWords) {
-          // We already have the starting prefix, see if this is a continuation of the code.
-          const remainingCode = verificationCode.substring(joinedWord.length)
-          if (remainingCode.lastIndexOf(word.text, 0) === 0) {
-            // New suffix, add to joined word.
-            joinedWord += word.text
-            if (joinedWord === verificationCode) {
-              // Entire word has been found!
-              verified = POI_STATUS.VERIFIED
-              verifiedConfidence = word.confidence
-              lineFound = lineNumber
-              wordFound = wordNumber
-              return {
-                verified,
-                verifiedConfidence,
-                lineFound,
-                wordFound,
-                verificationCode,
-              }
-            }
-          } else {
-            // Not a matching suffix, restart.
-            joinedWord = ''
-            joiningWords = false
-          }
-        } else if (verificationCode.lastIndexOf(word.text, 0) === 0) {
-          // Current word is a starting prefix.
-          joiningWords = true
-          joinedWord = word.text
-        }
-      }
-      if (word.text.includes('l')) {
-        word.text = word.text.replace('l', '1')
-        console.log('Trying again with l to 1 replacement.')
-        if (joinedWord) {
-          console.log(`${joinedWord} + ${word.text} === ${verificationCode}`)
-        } else {
-          console.log(`${word.text} === ${verificationCode}`)
-        }
-        // First, see if this word is the entire verification code.
-        if (word.text === verificationCode) {
-          verified = POI_STATUS.VERIFIED
-          verifiedConfidence = word.confidence
-          lineFound = lineNumber
-          wordFound = wordNumber
-          return {
-            verified,
-            verifiedConfidence,
-            lineFound,
-            wordFound,
-            verificationCode,
-          }
-        } else if (joiningWords) {
-          // We already have the starting prefix, see if this is a continuation of the code.
-          const remainingCode = verificationCode.substring(joinedWord.length)
-          if (remainingCode.lastIndexOf(word.text, 0) === 0) {
-            // New suffix, add to joined word.
-            joinedWord += word.text
-            if (joinedWord === verificationCode) {
-              // Entire word has been found!
-              verified = POI_STATUS.VERIFIED
-              verifiedConfidence = word.confidence
-              lineFound = lineNumber
-              wordFound = wordNumber
-              return {
-                verified,
-                verifiedConfidence,
-                lineFound,
-                wordFound,
-                verificationCode,
-              }
-            }
-          } else {
-            // Not a matching suffix, restart.
-            joinedWord = ''
-            joiningWords = false
-          }
-        } else if (verificationCode.lastIndexOf(word.text, 0) === 0) {
-          // Current word is a starting prefix.
-          joiningWords = true
-          joinedWord = word.text
-        }
-      }
-      // known OCr issue with g and 9, so check both just in case.
-      if (word.text.includes('g')) {
-        word.text = word.text.replace('g', '9')
-        console.log('Trying again with g to 9 replacement.')
-        if (joinedWord) {
-          console.log(`${joinedWord} + ${word.text} === ${verificationCode}`)
-        } else {
-          console.log(`${word.text} === ${verificationCode}`)
-        }
-        // First, see if this word is the entire verification code.
-        if (word.text === verificationCode) {
-          verified = POI_STATUS.VERIFIED
-          verifiedConfidence = word.confidence
-          lineFound = lineNumber
-          wordFound = wordNumber
-          return {
-            verified,
-            verifiedConfidence,
-            lineFound,
-            wordFound,
-            verificationCode,
-          }
-        } else if (joiningWords) {
-          // We already have the starting prefix, see if this is a continuation of the code.
-          const remainingCode = verificationCode.substring(joinedWord.length)
-          if (remainingCode.lastIndexOf(word.text, 0) === 0) {
-            // New suffix, add to joined word.
-            joinedWord += word.text
-            if (joinedWord === verificationCode) {
-              // Entire word has been found!
-              verified = POI_STATUS.VERIFIED
-              verifiedConfidence = word.confidence
-              lineFound = lineNumber
-              wordFound = wordNumber
-              return {
-                verified,
-                verifiedConfidence,
-                lineFound,
-                wordFound,
-                verificationCode,
-              }
-            }
-          } else {
-            // Not a matching suffix, restart.
-            joinedWord = ''
-            joiningWords = false
-          }
-        } else if (verificationCode.lastIndexOf(word.text, 0) === 0) {
-          // Current word is a starting prefix.
-          joiningWords = true
-          joinedWord = word.text
-        }
       }
     })
   })
---
+++ test/unit/ocr.test.js
@@ -0,0 +1,198 @@
+import { computerVisionFromFile } from '../../server-middleware/OCR'
+import { checkUpload } from '../../server-middleware/poi'
+import { POI_STATUS } from '../../store/types'
+const Path = require('path')
+const fs = require('fs')
+const nodeHtmlToImage = require('node-html-to-image')
+
+const LOOP_ITERATIONS = 5
+const TIMEOUT = 120000
+jest.setTimeout(TIMEOUT)
+
+// eslint-disable-next-line no-extend-native
+String.prototype.replaceAt = function (index, replacement) {
+  return (
+    this.substr(0, index) +
+    replacement +
+    this.substr(index + replacement.length)
+  )
+}
+
+function makeID(length) {
+  let result = ''
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  const charactersLength = characters.length
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+  return result
+}
+
+afterAll(() => {
+  // Clean up all created files.
+  fs.readdir(__dirname, (err, files) => {
+    if (err) throw err
+
+    for (const file of files) {
+      if (file.match(/.\.png/)) {
+        fs.unlink(Path.join(__dirname, file), (err) => {
+          if (err) throw err
+        })
+      }
+    }
+  })
+})
+
+test(`It Can OCR ${LOOP_ITERATIONS} good quality codes`, async () => {
+  // Loop X Times
+  const results = { pass: 0, fail: 0, confidenceAvg: 0 }
+  for (let i = 0; i < LOOP_ITERATIONS; i++) {
+    let code
+    if (i === 0) {
+      // Code with an O
+      let rndUUID = makeID(20)
+      rndUUID = rndUUID.replaceAt(0, 'O')
+      rndUUID = rndUUID.replaceAt(5, 'O')
+      code = rndUUID
+    } else if (i === 1) {
+      // Code with an l
+      let rndUUID = makeID(20)
+      rndUUID = rndUUID.replaceAt(0, 'L')
+      rndUUID = rndUUID.replaceAt(5, 'L')
+      code = rndUUID
+    } else if (i === 2) {
+      // Code with a g.
+      let rndUUID = makeID(20)
+      rndUUID = rndUUID.replaceAt(0, 'G')
+      rndUUID = rndUUID.replaceAt(5, 'G')
+      code = rndUUID
+    } else if (i === 3) {
+      let rndUUID = makeID(20)
+      rndUUID = rndUUID.replaceAt(0, 'G')
+      rndUUID = rndUUID.replaceAt(2, 'O')
+      rndUUID = rndUUID.replaceAt(4, 'L')
+      code = rndUUID
+    } else {
+      code = makeID(20)
+    }
+
+    try {
+      await nodeHtmlToImage({
+        output: Path.join(__dirname, `./${code}.png`),
+        html: `<html  style="
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      "><body    style="
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      "><h1 style="font-family: 'Inconsolata', sans-serif;">${code}</h1></body></html>`,
+      })
+      const filePath = Path.join(__dirname, `${code}.png`)
+
+      const ocrResult = await computerVisionFromFile(filePath)
+
+      const poiResult = await checkUpload(code, ocrResult[0])
+      if (poiResult.verified === POI_STATUS.FAILED) {
+        results.fail++
+      } else if (poiResult.verified === POI_STATUS.VERIFIED) {
+        results.pass++
+      }
+      results.confidenceAvg =
+        (results.confidenceAvg + poiResult.verifiedConfidence) / i + 1
+    } catch (e) {
+      console.error(e)
+      expect(false).toBe(true)
+    }
+  }
+  console.log(results)
+  expect(results.fail).toEqual(0)
+  expect(results.confidenceAvg).toBeGreaterThan(0.95)
+})
+
+test.skip(`It Can OCR ${LOOP_ITERATIONS} poor quality codes`, async () => {
+  // Loop X Times
+  const results = { pass: 0, fail: 0, confidenceAvg: 0 }
+  for (let i = 0; i < LOOP_ITERATIONS; i++) {
+    let code, poorlyFormattedCode
+
+    if (i === 0) {
+      // Code with an O
+      let rndUUID = makeID(20)
+      rndUUID = rndUUID.replaceAt(0, 'O')
+      rndUUID = rndUUID.replaceAt(5, 'O')
+      code = rndUUID
+      poorlyFormattedCode = code.replaceAt(4, ' ').replaceAt(5, ' ')
+    } else if (i === 1) {
+      // Code with an l
+      let rndUUID = makeID(20)
+      rndUUID = rndUUID.replaceAt(0, 'L')
+      rndUUID = rndUUID.replaceAt(5, 'L')
+      code = rndUUID
+      poorlyFormattedCode = code.replaceAt(2, ' ').replaceAt(9, ' ')
+    } else if (i === 2) {
+      // Code with a g.
+      let rndUUID = makeID(20)
+      rndUUID = rndUUID.replaceAt(0, 'G')
+      rndUUID = rndUUID.replaceAt(5, 'G')
+      poorlyFormattedCode = code.replaceAt(2, ' ')
+      code = rndUUID
+    } else if (i === 3) {
+      let rndUUID = makeID(20)
+      rndUUID = rndUUID.replaceAt(0, 'G')
+      rndUUID = rndUUID.replaceAt(2, 'O')
+      rndUUID = rndUUID.replaceAt(4, 'L')
+      poorlyFormattedCode = code.replaceAt(2, ' ').replaceAt(4, ' ')
+      code = rndUUID
+    } else {
+      code = makeID(20)
+      poorlyFormattedCode = code
+    }
+
+    try {
+      await nodeHtmlToImage({
+        output: Path.join(__dirname, `./${code}.png`),
+        html: `<html  style="
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      "><body    style="
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+      "><h1 style="font-family: 'Inconsolata', sans-serif;">${poorlyFormattedCode}</h1></body></html>`,
+      })
+      const filePath = Path.join(__dirname, `${code}.png`)
+
+      const ocrResult = await computerVisionFromFile(filePath)
+
+      const poiResult = await checkUpload(code, ocrResult[0])
+      if (poiResult.verified === POI_STATUS.FAILED) {
+        results.fail++
+      } else if (poiResult.verified === POI_STATUS.VERIFIED) {
+        results.pass++
+      }
+      results.confidenceAvg =
+        (results.confidenceAvg + poiResult.verifiedConfidence) / i + 1
+    } catch (e) {
+      console.error(e)
+      expect(false).toBe(true)
+    }
+  }
+  console.log(results)
+  expect(results.fail).toEqual(0)
+  expect(results.confidenceAvg).toBeGreaterThan(0.95)
+})
